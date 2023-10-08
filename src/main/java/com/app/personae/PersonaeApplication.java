8392fc1dea27e56b622086cba6e91899fa8c93a3
AzureAD\JakubCywiński <j.cywinski95@gmail.com>
2023-10-08 11:09:34 +0200

    Setting up a personae app

---
+++ src/main/java/com/app/personae/PersonaeApplication.java
@@ -0,0 +1,13 @@
+package com.app.personae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/personae/PersonaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.personae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
